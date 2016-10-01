7bc5efa228fa635c8f8d2d409098a00d2f0504f0
Jeremy Larter <jeremylarter@live.com.au>
2016-10-01 23:17:05 +1000

    initial build configuration

--- scripts/beware.blocks.ts
+++ scripts/beware.blocks.ts
@@ -1,12 +1,25 @@
 "use strict";
-//comments are automatically removed
+// when webpack is installed as a dev dependency, it can be run from a node prompt with node_modules\.bin\webpack
+// also node_modules\.bin\webpack-dev-server
+// http://localhost:8080/webpack-dev-server/index.htm
+// also node_modules\.bin\webpack-dev-server --inline
+// http://localhost:8080/
+import {Observable} from "rxjs";
+import {run} from "@cycle/rxjs-run";
+import {makeDOMDriver, h1} from "@cycle/dom";
 
-class Startup {
-
-    public static main(): number {
-        console.log('Hello World');
-        return 0;
-    }
+function main() {
+  const a = "test built 8";
+  const sinks = {
+    DOM: Observable.interval(1000).map(i =>
+      h1("" + i + " seconds elapsed")
+    )
+  };
+  return sinks;
 }
 
-Startup.main();
+const drivers = {
+  DOM: makeDOMDriver("#app")
+};
+
+run(main, drivers);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/* jslint
+    node
+*/
+module.exports = {
+    entry: "./scripts/beware.blocks.ts",
+    output: {
+        filename: "built/bundle.js",
+        sourcePrefix: ""
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: "tslint"
+            }
+        ],
+        loaders: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: "ts-loader"
+            }
+        ],
+
+        resolve: {
+            extensions: ['', '.js', '.ts']
+        }
+    }
+};
